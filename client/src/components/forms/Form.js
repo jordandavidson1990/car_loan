56de30e8b304487feb968140dcb9afaf56ccadd5
Jordan Davidson <jordandavidson1990@hotmail.com>
2020-01-14 10:39:10 +0000

    fixed startFee bug

--- client/src/components/forms/Form.js
+++ client/src/components/forms/Form.js
@@ -19,22 +19,6 @@ function Form({ vehiclePrice, calculateLoanScheme }) {
     }
 
     const makeVehicle = (target) => {
-        let startFee = () => {
-            if (!target.startFee.value) {
-                startFee = 88
-            }
-            else {
-                startFee = target.startFee.value
-            }
-        }
-        let endFee = () => {
-            if (!target.endFee.value) {
-                endFee = 20
-            }
-            else {
-                endFee = target.endFee.value
-            }
-        }
         const newVehicle = ({
             price: vehiclePrice,
             depositAmount: target.depositAmount.value,
@@ -42,8 +26,8 @@ function Form({ vehiclePrice, calculateLoanScheme }) {
             financeOption: target.financeOption.value,
             minMonthly: target.minMonthly.value,
             maxMonthly: target.maxMonthly.value,
-            startFee: { startFee },
-            endFee: { endFee }
+            startFee: target.startFee.value,
+            endFee: target.endFee.value
         })
         calculateLoanScheme(newVehicle);
     }
